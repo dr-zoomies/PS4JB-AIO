1c18aaa64a982ba0a8abf2ed085c95f77b105c83
drcold <polyang.0323@gmail.com>
2026-09-24 13:51:32 +0800

    Update utils.js

--- 700/module/utils.js
+++ 700/module/utils.js
@@ -41,8 +41,9 @@ function getLogConsole() {
     el.id = "console";
     el.setAttribute(
       "style",
-      "position:absolute;left:-9999px;top:-9999px;width:1px;height:1px;" +
-        "overflow:hidden;opacity:0;pointer-events:none;"
+      "position:fixed;left:5%;top:15%;width:90%;height:65%;" +
+    "overflow:auto;background:#000;color:#00ff00;padding:15px;" +
+    "font-size:16px;white-space:pre-wrap;z-index:9999;"
     );
     if (document.body) {
       document.body.appendChild(el);
